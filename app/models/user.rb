6eb5c159d02945697829eab05b53ef890443fc96
Julie Rivest <julierivest93@gmail.com>
2017-08-08 16:01:55 +0000

    gems installed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,11 @@
 class User < ActiveRecord::Base
-
   has_many :reviews
-
   has_secure_password
-
   validates :email, uniqueness: true
   validates :first_name, :last_name, :password, :password_confirmation, :email, presence: true
   validates :password, length: { minimum: 6 }
-
   def self.authenticate_with_credentials(email, password)
     user = User.where('lower(email) = lower(?)', email.strip).first
-
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(password)
       return user
@@ -18,5 +13,4 @@ class User < ActiveRecord::Base
       return nil
     end
   end
-
 end
